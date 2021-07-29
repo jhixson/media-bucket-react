740459b921463c0465eb31811728f37bd236e8f2
Jesse Hixson <hixsonj@gmail.com>
2021-07-29 07:13:50 -0400

    Adds grid around cards

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,27 +1,22 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 import MediaItem from "./components/MediaItem";
 import { useGetItemsQuery } from "./__generated__/graphql/getItems.types";
 
-const useStyles = makeStyles({
-  list: {
-    listStyle: "none",
-    margin: 0,
-    padding: 0,
-  },
-});
-
 const Home: React.FC = () => {
-  const classes = useStyles();
   const { data, loading } = useGetItemsQuery();
   const itemList = loading
     ? []
     : data?.items?.map((item) => (
-        <li key={item?.title}>
+        <Grid key={item?.title} item>
           <MediaItem title={item?.title} status={item?.status} />
-        </li>
+        </Grid>
       ));
-  return <ul className={classes.list}>{itemList}</ul>;
+  return (
+    <Grid container justifyContent="center" spacing={4} xs={12}>
+      {itemList}
+    </Grid>
+  );
 };
 
 export default Home;
